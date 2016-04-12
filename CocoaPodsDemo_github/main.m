897b9f561dfec7e2074489b514fb511723e8dc03
blackJokerZhou <luofeikeji@blackjokerdemacbook-pro.local>
2016-04-12 11:49:08 +0800

    Initial Commit

---
+++ CocoaPodsDemo_github/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CocoaPodsDemo_github
+//
+//  Created by blackJokerZhou on 16/4/12.
+//  Copyright © 2016年 blackJokerZhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ CocoaPodsDemo_githubTests/CocoaPodsDemo_githubTests.m
@@ -0,0 +1,39 @@
+//
+//  CocoaPodsDemo_githubTests.m
+//  CocoaPodsDemo_githubTests
+//
+//  Created by blackJokerZhou on 16/4/12.
+//  Copyright © 2016年 blackJokerZhou. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CocoaPodsDemo_githubTests : XCTestCase
+
+@end
+
+@implementation CocoaPodsDemo_githubTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
